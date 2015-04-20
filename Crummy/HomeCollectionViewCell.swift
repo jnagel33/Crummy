3736820617ead64667dea66b086f6f06881d99da
Josh Nagel <jnagel33@gmail.com>
2015-04-20 10:31:40 -0700

    Adde basic collection view functionality

--- Crummy/HomeCollectionViewCell.swift
+++ Crummy/HomeCollectionViewCell.swift
@@ -10,4 +10,6 @@ import UIKit
 
 class HomeCollectionViewCell: UICollectionViewCell {
     
+  @IBOutlet weak var personImageView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
 }
--- Crummy/HomeViewController.swift
+++ Crummy/HomeViewController.swift
@@ -10,21 +10,27 @@ import UIKit
 
 class HomeViewController: UIViewController, UICollectionViewDataSource {
 
+  @IBOutlet weak var collectionView: UICollectionView!
+  
+  var people = [Person(theName: "Josh", theDOB: NSDate(), theInsuranceID: "130831", theConsultantPhone: "8010380024"), Person(theName: "Randy", theDOB: NSDate(), theInsuranceID: "244553", theConsultantPhone: "4200244244"), Person(theName: "Ed", theDOB: NSDate(), theInsuranceID: "43988305", theConsultantPhone: "94835553")]
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    self.collectionView.dataSource = self
   }
   
   //MARK:
   //MARK: UICollectionViewDataSource
   
   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 5
+    return people.count
   }
   
   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCellWithReuseIdentifier("HomeCell", forIndexPath: indexPath) as! HomeCollectionViewCell
-    
+    cell.nameLabel.text = people[indexPath.row].name
+    cell.personImageView.image = UIImage(named: "PersonPlaceholderImage")
     
     return cell
   }
