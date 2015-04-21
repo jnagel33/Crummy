93c59af517e46a87e67af425e0312e101f42b270
Randy McLain <Randoramma@me.com>
2015-04-21 08:25:39 -0700

    Pull morning of 4/21 with Josh's icon changes.

--- Crummy/EditKidViewController.swift
+++ Crummy/EditKidViewController.swift
@@ -40,4 +40,45 @@ class EditKidViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  @IBAction func donePressed(sender: AnyObject) {
+    
+  } // donePressed
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+  // func to set the date from the picker if no date is set.  
+  // https://github.com/ioscreator/ioscreator/blob/master/IOSSwiftDatePickerTutorial/IOSSwiftDatePickerTutorial/ViewController.swift
+  func datePickerChanged(datePicker:UIDatePicker) {
+    var dateFormatter = NSDateFormatter()
+    
+    dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
+    dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+    
+    var strDate = dateFormatter.stringFromDate(datePicker.date)
+    selectedKid.DOBString = strDate
+  }
+  
+  // pragma MARK: UIPickerViewDataSource Delegate
+  
+  func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  /* func pickerView(pickerView: UIPickerView, numberOfRowsInComponent: component) {
+  return colors.count
+  }*/
+  
+  func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return 1
+  }
+  
+  // pragma MARK: UIPickerViewDelegate
+//  
+//  func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+//    return colors[row]
+//  }
 }
