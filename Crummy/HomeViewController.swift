5dccb8bb01f45e2fad9aa5d30c167518d435f8e4
Randy McLain <Randoramma@me.com>
2015-04-24 08:32:36 -0700

    phone needs nibs added and done button added.

--- Crummy/HomeViewController.swift
+++ Crummy/HomeViewController.swift
@@ -8,19 +8,27 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UICollectionViewDataSource {
-
+class HomeViewController: UIViewController, UICollectionViewDataSource, UITableViewDataSource, UITableViewDelegate {
+  
   @IBOutlet weak var collectionView: UICollectionView!
   
+  
+  let kidNumberHeight: CGFloat = 50.0
   let crummyApiService = CrummyApiService()
   var kids = [Kid(theName: "Josh", theDOB: "2014-10-10", theInsuranceID: "130831", theNursePhone: "8010380024"), Kid(theName: "Randy", theDOB: "2014-10-10", theInsuranceID: "244553", theNursePhone: "4200244244"), Kid(theName: "Ed", theDOB: "2014-10-10", theInsuranceID: "43988305", theNursePhone: "94835553"), Kid(theName: "Josh", theDOB: "2014-10-10", theInsuranceID: "130831", theNursePhone: "8010380024"), Kid(theName: "Randy", theDOB: "2014-10-10", theInsuranceID: "244553", theNursePhone: "4200244244"), Kid(theName: "Ed", theDOB: "2014-10-10", theInsuranceID: "43988305", theNursePhone: "94835553")]
   
   // Randy is working on this...
   let phonePopoverAC = UIAlertController(title: "PhoneList", message: "Select a number to dial.", preferredStyle: UIAlertControllerStyle.ActionSheet)
+  // find the Nib in the bundle.
+  let phoneNib = UINib(nibName: "PhoneTableViewCell", bundle: NSBundle.mainBundle())
   override func viewDidLoad() {
     super.viewDidLoad()
     self.collectionView.dataSource = self
-    }
+    
+    
+    //    self.phoneMenuView.delegate = self
+    //    self.phoneMenuView.dataSource = self
+  }
   
   //MARK:
   //MARK: UICollectionViewDataSource
@@ -37,7 +45,7 @@ class HomeViewController: UIViewController, UICollectionViewDataSource {
     return cell
   }
   
-  //MARK: 
+  //MARK:
   //MARK: prepareForSegue
   
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -51,18 +59,43 @@ class HomeViewController: UIViewController, UICollectionViewDataSource {
     }
   }
   
-  //MARK: 
-  //MARK: - popover VC. 
+  //MARK:
+  //MARK: - popover VC.
   
-  // Randy is working on this.. 
+  // Randy is working on this..
   
   @IBAction func phoneButtonPressed(sender: AnyObject) {
+    // adding table view properties for the phone table view popover.
     
+    var kidCount = CGFloat(kids.count)
     
+    println("button pressed")
+    var phoneMenuView : UITableView!
+    let phoneMenuViewHeight: CGFloat =  CGFloat(self.view.frame.height) - (kidCount * kidNumberHeight)
+    phoneMenuView = UITableView(frame: CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: -phoneMenuViewHeight))
+    phoneMenuView.registerNib(phoneNib, forCellReuseIdentifier: "phoneCell")
     
-    
-  }
+    UIView.animateWithDuration(1.4, animations: { () -> Void in
+      self.view.addSubview(phoneMenuView)
+    })
+  } // phoneButtonPressed
   
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCellWithIdentifier("phoneCell", forIndexPath: indexPath) as! PhoneTableViewCell
+    
+    cell.Name.text = kids[indexPath.row].name
+    cell.InsuranceID.text = kids[indexPath.row].insuranceId
+    cell.Phone.text = kids[indexPath.row].nursePhone
+    
+    return cell
+  } // cellForRowAtIndexPath
   
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return kids.count
+  } // numberOfRowsInSection
   
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    UIApplication.sharedApplication().openURL(NSURL(fileURLWithPath: "tel://\(kids[indexPath.row].nursePhone)")!)
+  } // didSelectRowAtIndexPath
 }
