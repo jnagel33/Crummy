b954f9304367eaf4cb889cc1fa8ed2be6c9fb442
Josh Nagel <jnagel33@gmail.com>
2015-04-24 19:08:08 -0700

    Add photo feature to edit kid page. No way to save as of now

---
+++ Crummy/ImagePickerCell.swift
@@ -0,0 +1,15 @@
+//
+//  PickerImageTableViewCell.swift
+//  Crummy
+//
+//  Created by Josh Nagel on 4/24/15.
+//  Copyright (c) 2015 CF. All rights reserved.
+//
+
+import UIKit
+
+class ImagePickerCell: UITableViewCell {
+  
+  @IBOutlet weak var kidImageView: UIImageView!
+
+}
